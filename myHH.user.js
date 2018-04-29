c5620c151d951ecbd3b526cf16b5aa68e25cbd02
Chifumy <37037687+Chifumy@users.noreply.github.com>
2018-04-29 20:30:38 +0200

    Update myHH.user.js

--- myHH.user.js
+++ myHH.user.js
@@ -12,15 +12,23 @@
 
 var myHH= {
 	general: (a)=> {
-		if(a[0]=='boss') return;
-		localStorage.setItem('curWorld', Hero.infos.questing.id_world);
 		$('div[rel^="content"] > div > a[href^="/quest"]').after($('<a/>', {
+			'id': 'myHH_boss',
 			'href': '/boss',
+			'onclick': 'return false;',
 			'html': [
 				$('<ic/>', {'class': 'continue_quest'}),
 				$('<span/>', {'text': 'Boss'})
 			]
 		}));
+		$('#myHH_boss').click(()=> {
+			$('body').append($('<div/>', {
+				'css': {
+					height: '90%',
+					width: '95%'
+				}
+			}));
+		});
 	},
 	activities: (a)=> {
 		$('.missions_wrap > div.mission_object').sort((a,b)=> {
@@ -42,71 +50,6 @@ var myHH= {
 			for(var a=0;a<3;a++)
 				window.open(`https://www.hentaiheroes.com/battle.html?id_arena=${a}`);
 		});
-	},
-	boss: ()=> {
-		$('head title').text('Hentai Heroes');
-		let boss= ["Dark Lord", "l'Espion Ninja", "Gruntt", "Edwarda", "Donatien", "Sylvanus", "Bremen", "Finalmecia", "Fredy Sih Roko Senseï"];
-		let idOB= localStorage.getItem("curWorld")-1;
-		$('body').html('');
-		for(let a=0;a<idOB;a++) {
-			$('body').append($('<div/>', {
-				'class': 'troll',
-				'html': [
-					$('<h2/>', {'text': boss[a]}),
-					$('<img/>', {'src': `https://content.hentaiheroes.com/pictures/trolls/${a+1}/ava1.png`}),
-					$('<a/>', {
-						'text': 'Combat',
-						'href': `https://www.hentaiheroes.com/battle.html?id_troll=${a+1}`
-					})
-				]
-			}));
-		}
-		$('body').append($('<style/>', {
-'text': `body {
-	background: #301 fixed;
-	background-image: url(https://content.hentaiheroes.com/design/bg_pattern2.png), linear-gradient(to bottom,#402,#101);
-	background-size: 100px 100px, 100% 100%;
-	display: flex;
-	flex-direction: row;
-	flex-wrap: nowrap;
-	justify-content: center;
-	align-content: center;
-	align-items: center;
-	margin: 0px;
-	padding: 0px;
-}
-body > div.troll {
-	max-height: 90%;
-}
-body > div.troll > h2 {
-	position: relative;
-}
-body > div.troll > img {
-	display: block;
-	height: 90%;
-	margin: 0px 26px;
-	image-rendering: -webkit-optimize-contrast;
-	margin-top: 22px;
-}
-body > div.troll > a {
-	position: relative;
-	display: block;
-	background-image: linear-gradient(#00aaff 0px, #006688 50%, #005577 51%, #00aaff 100%);
-	border: 1px solid #006688;
-	border-radius: 7px;
-	box-shadow: #ffffffcc -1px 2px 1px 1px inset, #005577 1px -2px 1px 3px inset, #003344b3 0px 3px 3px 0px;
-	color: #ffffff;
-	cursor: pointer;
-	font-family: "Carter One", cursive;
-	font-size: 18px;
-	font-weight: 400;
-	height: 42px;
-	line-height: 42px;
-	text-align: center;
-	width: 100px;
-	text-decoration: none;
-}`
-		}));
 	}
 };
 
